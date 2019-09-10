77c8c5c91077eb04f6e5937378aeee3625362371
SHUBHAMDaa(^_^) <SHUBHAMDaa(^_^)@DESKTOP-U9QO90T>
2019-09-10 17:48:14 +0530

    Added First Class

--- GitTest/src/GitTest2.java
+++ GitTest/src/GitTest2.java
@@ -1,6 +1,7 @@
 
  class GitTest2 extends GitTes3{
-	    {
+	   class GitTest3 extends GitTest4 {
+		   
 		
 	}
 
--- GitTest/src/GitTest4.java
+++ GitTest/src/GitTest4.java
@@ -1,4 +1,12 @@
 
 public class GitTest4 {
+	 static int k;
 
-}
+	public static void main(String args[]) {
+		
+		GitTes3 obj=new GitTes3();
+				
+			int	obj1=(int) k;
+		
+		
+}}
